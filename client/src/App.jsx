5eb6d46c5461c3abdddd82d081706ea06f1eeb6e
usama <princeusamaansari15@gmail.com>
2022-01-19 23:03:54 +0500

    web 3.0 init

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Navbar, Welcome, Footer, Services, Transaction } from './component'
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+     <div className="gradient-bg-welcome">
+       <Navbar/>
+       <Welcome/>
+     </div>
+     <Services/>
+     <Transaction/>
+     <Footer/>
+    </div>
+  )
+}
+
+export default App
